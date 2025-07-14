1c25d50832e449689f6219fede501ec590f764e2
Nafiuls <snafiul700@gmail.com>
2025-07-14 20:35:29 +0600

    created pages and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import DashBoard from "./pages/DashBoard";
+import AuthPage from "./pages/AuthPage";
+import LinkPage from "./pages/LinkPage";
+import RedirectLinkPage from "./pages/RedirectLinkPage";
+import AppLayout from "./layout/AppLayout";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashBoard />,
+      },
+      {
+        path: "/auth",
+        element: <AuthPage />,
+      },
+      {
+        path: "/link/:id",
+        element: <LinkPage />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLinkPage />,
+      },
+    ],
+  },
+]);
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
