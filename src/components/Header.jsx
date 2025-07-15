91eb4a3d903307eace6460a2c0ae39e1777f4e42
Nafiuls <snafiul700@gmail.com>
2025-07-15 12:36:01 +0600

    landing page done alhamdulillah

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ import { LinkIcon, LogOut } from "lucide-react";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = true;
+  const user = false;
 
   return (
     <nav className="flex items-center justify-between py-4">
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl font-extrabold text-center text-white">
+        The only URL Shortener <br /> you&rsquo;ll ever need!👇
+      </h2>
+      <form className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+        <Input
+          type={"url"}
+          placeholder="Enter your loooooong URL"
+          className={"h-full flex-1 p-4"}
+        />
+        <Button
+          className={"h-full"}
+          size={"lg"}
+          variant={"destructive"}
+          type="submit"
+        >
+          Shorten!
+        </Button>
+      </form>
+      <img src="/banner.jpeg" alt="Banner" className="w-full md:px-11 my-11" />
+    </div>
+  );
 };
 
 export default LandingPage;
