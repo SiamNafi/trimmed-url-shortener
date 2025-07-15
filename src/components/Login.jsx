711e6a025cdf5ae6025d2376a8b1ac71e288c160
Nafiuls <snafiul700@gmail.com>
2025-07-15 13:47:26 +0600

    added form validation

---
+++ src/components/Login.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { BeatLoader } from "react-spinners";
+import * as Yup from "yup";
+import Error from "./Error";
+
+const Login = () => {
+  const loading = false;
+  const [error, seterror] = useState([]);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleLogin = async () => {
+    seterror([]);
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email("Invalid Email")
+          .required("Email is Required"),
+        password: Yup.string()
+          .min(6, "Password must be at least 6 characters")
+          .required("Password is Required"),
+      });
+      await schema.validate(formData, { abortEarly: false });
+      //   api call
+    } catch (error) {
+      const newError = {};
+      error?.inner?.forEach((err) => {
+        newError[err.path] = err.message;
+      });
+      seterror(newError);
+    }
+  };
+  return (
+    <div>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className={"text-3xl"}>Login </CardTitle>
+          <CardDescription>
+            to your account if you already have one
+          </CardDescription>
+          <Error message={"Some Error"} />
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  onChange={handleInputChange}
+                  required
+                />
+                {error.email && <Error message={error.email} />}
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  name="password"
+                  type="password"
+                  required
+                  placeholder="********"
+                  onChange={handleInputChange}
+                />
+                {error.password && <Error message={error.password} />}
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button onClick={handleLogin} type="submit" className="w-full">
+            {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Login from "@/components/Login";
+import Signup from "@/components/Signup";
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [searchParams] = useSearchParams();
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchParams.get("createNew")
+          ? "Hold up! Let's Login First..."
+          : "Login / Signup"}
+      </h1>
+      {/* tabs */}
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className={"grid w-full grid-cols-2"}>
+          <TabsTrigger className={"cursor-pointer"} value="login">
+            Login
+          </TabsTrigger>
+          <TabsTrigger className={"cursor-pointer"} value="signup">
+            Signup
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="signup">
+          <Signup />{" "}
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default AuthPage;
